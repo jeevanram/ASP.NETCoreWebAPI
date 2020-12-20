dcfd2319d36d110ed4f71c7b41b7f5ead6a1b7fc
Jeevanram <mailjeevanram@gmail.com>
2020-12-20 11:50:50 -0600

    Docker #1 : Sample Docker Console App

---
+++ DockerConsoleApp/DockerConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DockerConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int counter = 0;
+            int max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+            while(max == -1 || counter < max)
+            {
+                Console.WriteLine($"Counter: {++counter}");
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ DockerConsoleApp/DockerConsoleApp/README.md
@@ -0,0 +1,8 @@
+﻿Reference
+https://docs.microsoft.com/en-us/dotnet/core/docker/build-container?tabs=windows
+
+STEPS TO CREATE DOCKER CONSOLE APP
+1) CREATE A CONSOLE APP THAT TAKES ARGUMENT TO MAIN METHOD AND RUNS A COUNTER
+2) CREATE A DOCKER FILE
+	a) Dockerfile is used by the docker build to create a container image.
+	b) 
